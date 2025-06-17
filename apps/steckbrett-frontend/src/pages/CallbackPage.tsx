72478c6041e3e8347299cac699c225c2b2163f19
mherkert <marion.herkert@gmx.de>
2025-06-17 22:08:47 +0200

    back to original callback page

--- apps/steckbrett-frontend/src/pages/CallbackPage.tsx
+++ apps/steckbrett-frontend/src/pages/CallbackPage.tsx
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export function CallbackPage() {
   const { isLoading, error, user } = useAuth0();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  useEffect(() => {}, []);
-  // useEffect(() => {
-  //   if (!isLoading) {
-  //     navigate("/"); // or wherever you want to go after login
-  //   }
-  // }, [isLoading, navigate]);
+  useEffect(() => {
+    if (!isLoading) {
+      navigate("/"); // or wherever you want to go after login
+    }
+  }, [isLoading, navigate]);
 
   console.log({ user });
   if (error) return <div>Oops: {error.message}</div>;
-  return <div>{isLoading ? "Loading..." : "Redirecting..."}</div>;
+  return <div>Loading...</div>;
 }
